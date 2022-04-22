4bff3a1147890c008e93773edb3f7a932245c4ca
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-04-22 18:14:42 +0200

    lights and mouse controls

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import './assets/css/style.css'
 //like a container that holds all objects
 import * as THREE from 'three';
 import {color} from "three/examples/jsm/nodes/ShaderNode";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -21,17 +22,43 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.OctahedronGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
-const octa = new THREE.Mesh(geometry, material);
 
-scene.add(octa);
+//octa1
+const geometry1 = new THREE.OctahedronGeometry(10, 3);
+const material1 = new THREE.MeshStandardMaterial({color: 0xFFFFFF, wireframe: true});
+const octa1 = new THREE.Mesh(geometry1, material1);
+
+//octa1
+let size = 4;
+const geometry2 = new THREE.TetrahedronGeometry(size, 1);
+const material2 = new THREE.MeshStandardMaterial({color: 0x664477});
+const octa2 = new THREE.Mesh(geometry2, material2);
+
+scene.add(octa1);
+scene.add(octa2);
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(10, 10, 10);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(pointLight, ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
-    octa.rotation.x += 0.01;
-    octa.rotation.y += 0.005;
-    octa.rotation.z += 0.01;
+    octa1.rotation.x += 0.01;
+    octa1.rotation.y += 0.005;
+    octa1.rotation.z += 0.01;
+
+    octa2.rotation.x -= 0.01;
+    octa2.rotation.y -= 0.005;
+    octa2.rotation.z -= 0.01;
+
+    controls.update;
+
     renderer.render(scene, camera);
 }
 
